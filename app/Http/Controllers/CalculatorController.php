919d6dc3b320b7b9a3fb640d22e730d5ec79641c
NextTech-coder <172751968+NextTech-coder@users.noreply.github.com>
2025-01-17 22:38:48 +0000

    Apply automatic changes

--- app/Http/Controllers/CalculatorController.php
+++ app/Http/Controllers/CalculatorController.php
@@ -9,11 +9,6 @@ class CalculatorController extends Controller
 {
     public function show(Calculator $calculator, $input = null)
     {
-
-
-
-
-
         return view('calculator', [
             'data' => base64_decode(str_replace('.', ' /', $input)),
         ]);
